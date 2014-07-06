77d050a6a8a0030396f3393e72c1942f21376590
yamaguchi-e <emi.yamaguchi86@gmail.com>
2014-07-06 11:46:34 +0900

    例外が発生しないようチェックを追加(#27)

--- Kadai201403/src/jp/ktsystem/kadai201403/e_yamaguchi/Kadai.java
+++ Kadai201403/src/jp/ktsystem/kadai201403/e_yamaguchi/Kadai.java
@@ -335,19 +335,21 @@ public class Kadai {
 							continue;
 						} else {
 							oneRecord = oneRecord.replace(KadaiConstants.DELIMITER, KadaiConstants.BLANK_CHAR);
+
+							// 文字列が空文字の場合エラー
+							if (oneRecord.trim().isEmpty()) {
+								throw new KadaiException(KadaiConstants.INPUT_NULL_OR_BLANK_ERROR);
+							}
 						}
 
-						try {
-							int index = oneRecord.indexOf(KadaiConstants.ITEM_AND_VALUE_DELIMITER);
-							String key = KadaiUtil.obtainDate(oneRecord, KadaiConstants.LV2_ITEM_NAME_START_POSITION,
-									index - KadaiConstants.ITEM_NAME_END_POSITION);
-							String value = KadaiUtil.obtainDate(oneRecord, index - KadaiConstants.VALUE_START_POSITION);
+						int index = oneRecord.indexOf(KadaiConstants.ITEM_AND_VALUE_DELIMITER);
+						String key = KadaiUtil.obtainDate(oneRecord, KadaiConstants.LV2_ITEM_NAME_START_POSITION,
+								index - KadaiConstants.ITEM_NAME_END_POSITION);
+						String value = KadaiUtil.obtainDate(oneRecord, index - KadaiConstants.VALUE_START_POSITION);
 
-							workTimeMap.put(key, value);
-							count++;
-						} catch (IndexOutOfBoundsException iob) {
-							throw new KadaiException(KadaiConstants.INPUT_CONTROL_ERROR);
-						}
+						workTimeMap.put(key, value);
+						count++;
+
 						continue;
 					}
 
